6a567ea05194f4399ad8533098b0e1636694e23b
kouamschekina <kouamdelphine82@gmail.com>
2024-04-09 15:11:30 +0100

    Setup code

--- src/main/java/com/schekina/ToDo_app/controller/TodoCtroller.java
+++ src/main/java/com/schekina/ToDo_app/controller/TodoCtroller.java
@@ -1,9 +1,13 @@
 package com.schekina.ToDo_app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.schekina.ToDo_app.service.TodoService;
 
 @RestController
 public class TodoCtroller {
-
+    @Autowired
+    private TodoService todoService;
     // TODO create an instance of the service class  for eg Private TodoService todoService;
 }
--- src/main/java/com/schekina/ToDo_app/domain/Todo.java
+++ src/main/java/com/schekina/ToDo_app/domain/Todo.java
@@ -4,13 +4,33 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Table(name = "Todo list")
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    // TODO create getters and setters or you can use the lombok also set the table name
+
+    @Getter
+    @Setter
     private Long id;
+
+    @Getter
+    @Setter
     private String name;
 
-    // TODO create getters and setters or you can use the lombok also set the table name
+    public Todo(String name, Long id) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("TodoList={id='%d', name='%s'}",id ,name);
+    }
+
 }
