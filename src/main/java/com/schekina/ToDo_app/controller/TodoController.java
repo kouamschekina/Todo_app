3238c3aaece15346b40f1379004602846c9e9fe5
kouamschekina <kouamdelphine82@gmail.com>
2024-04-10 09:24:41 +0100

    feat: Implemented the todo service and controller

--- src/main/java/com/schekina/ToDo_app/controller/TodoController.java
+++ src/main/java/com/schekina/ToDo_app/controller/TodoController.java
@@ -1,12 +1,35 @@
 package com.schekina.ToDo_app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.schekina.ToDo_app.domain.Todo;
 import com.schekina.ToDo_app.service.TodoService;
 
 @RestController
 public class TodoController {
     @Autowired
     private TodoService todoService;
+
+    @GetMapping("/todos")
+    public List<Todo> getAllTodos(){
+        return todoService.getAllTodos();
+    }
+
+    @PostMapping("/todos")
+    public Todo addTodo(@RequestBody Todo todo){
+        return todoService.addTodo(todo);
+    }
+    @DeleteMapping("/todos")
+    public ResponseEntity<Void> deleteTodo(@RequestBody Todo todo){
+        todoService.deleteTodo(todo);
+        return ResponseEntity.ok().build();
+    }
 }
